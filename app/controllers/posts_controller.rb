b4be55899b05d3166d48557203a7d15ae21b35b1
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-06-07 16:50:09 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update]
 
   def index
-    @posts = Post.all
+    @postss = Post.all
   end
 
   def show
